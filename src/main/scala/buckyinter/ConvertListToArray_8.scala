c7b8d5186f97153ec7cecb8dc2788cf564e9df73
sopier <sopier@gmail.com>
2014-07-09 13:29:56 +0700

    Example of Converting Array to List, vice verca

---
+++ src/main/scala/buckyinter/ConvertListToArray_8.scala
@@ -0,0 +1,30 @@
+package buckyinter
+
+import java.util._
+
+class ConvertListToArray_8 {
+  def act = {
+    val stuff = Array("babies", "watermelon", "melons", "fudge")
+    val thelist = stuff.iterator.toList
+
+    // add to the first index
+    val thelist2 = thelist.::("first")
+    // add to the last index
+    val thelist3 = thelist2.reverse.::("end").reverse
+    
+    // convert back to an Array
+    val stuff2 = thelist3.toArray
+    
+    // loop through
+    for(s <- stuff2) {
+      printf("%s ", s)
+    }
+  }
+}
+
+object Main {
+  def main(args: Array[String]) {
+    val c = new ConvertListToArray_8
+    c.act
+  }
+}
